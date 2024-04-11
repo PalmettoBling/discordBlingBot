0623194a5a04a1864df73e38a27b68f2d935615e
Merrick Moss <palmettobling@gmail.com>
2024-04-11 10:58:00 -0400

    Fix null check for quoteId in app.http('quote')

--- src/functions/quote.js
+++ src/functions/quote.js
@@ -31,7 +31,7 @@ app.http('quote', {
         context.info("App ID: " + applicationId + " and Interaction ID: " + interactionToken);
 
         // Getting random number for quote if no options are provided
-        if ((!quoteId) && (quoteId !== "0")) {
+        if ((!quoteId) || (quoteId !== "0")) {
             context.info("No quote ID found, generating random quote ID...");
             const querySpec = {
                 query: "SELECT VALUE COUNT(1) FROM c"
